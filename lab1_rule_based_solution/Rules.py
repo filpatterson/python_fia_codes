e81c1710caa52d840be8ef4da0f5364b8d1dde9a
filpatterson <filpatterson10@gmail.com>
2021-02-03 13:11:47 +0200

    lab 1: complete rule-based working system

--- lab1_rule_based_solution/Rules.py
+++ lab1_rule_based_solution/Rules.py
@@ -2,19 +2,38 @@ from RuleHandler import RuleManager
 from RuleSystem import If, And, Or, Statement
 
 rulesDB = [
-    And(["has no hair", "has foggy eyes", "has no ears", "has round head", Or(["speaks universal", "speaks high elfian"])]).then("it is a genie"),
-    Or([And(["no human", "no man"]), And(["no hair", "no eyes"])]).then("monster")
+    
+    And(
+        ["has no hair", "has foggy eyes", "has no ears", "has round head", Or(["speaks force", "speaks high elfian"]), "has blue foggy body"]
+    ).then("genie"),
+    
+    And(
+        [Or(["has black hair", "has brown hair"]), Or(["has green eyes", "has brown eyes", "has gray eyes"]), "has small round ears", "has round head", "speaks universal", "has white skin body"]
+    ).then("mandalorian"),
+    
+    And(
+        [Or(["has black hair", "has brown hair"]), "has blue eyes", "has small round ears", "has round head", Or(["speaks universal", "speaks force"]), Or(["has white skin body", "has yellow skin body", "has black skin body"])]
+    ).then("jedi"),
+    
+    And(
+        ["has yellow hair", "has blue eyes", "has medium cubic ears", "has cubic head", Or(["speaks universal", "speaks tatooinean"]), "has black skin body"]
+    ).then("tatooiner"),
+    
+    And(
+        ["has white hair", Or(["has white eyes", "has black eyes"]), "has medium cubic ears", "has round head", Or(["speaks lunar", "speaks tatooinean", "speaks force"]), "has white skin body"]
+    ).then("loonie")
 ]
 
 ruleManager = RuleManager(rulesDB)
 
-userConditions = [
-    "has no hair", "has foggy eyes", "has no ears", "has round head", "speaks universal", "no man", "no human"
-]
+possibleGenieConditions = ["has no hair", "has foggy eyes", "has no ears", "has round head", "speaks high elfian", "has blue foggy body"]
+possibleMandalorianConditions = ["has brown hair", "has green eyes", "has small round ears", "has round head", "speaks universal", "has white skin body"]
+possibleJediConditions = ["has brown hair", "has blue eyes", "has small round ears", "has round head", "speaks force", "has white skin body"]
+possibleTatooinerConditions = ["has yellow hair", "has blue eyes", "has medium cubic ears", "has cubic head", "speaks tatooinean", "has black skin body"]
+possibleLoonieConditions = ["has white hair", "has white eyes", "has medium cubic ears", "has round head", "speaks lunar", "has white skin body"]
 
-userAnotherConditions = [
-    "no human", "no man"
-]
-
-print(ruleManager.give_best_match(userConditions))
-print(ruleManager.give_best_match(userAnotherConditions))
+print("Possible genie was considered by system as ", ruleManager.give_best_match(possibleGenieConditions))
+print("Possible mandalorian was considered by system as ", ruleManager.give_best_match(possibleMandalorianConditions))
+print("Possible jedi was considered by system as ", ruleManager.give_best_match(possibleJediConditions))
+print("Possible tatooiner was considered by system as ", ruleManager.give_best_match(possibleTatooinerConditions))
+print("Possible loonie was considered by system as ", ruleManager.give_best_match(possibleLoonieConditions))
